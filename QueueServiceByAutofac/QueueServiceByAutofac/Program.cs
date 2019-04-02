037290e5e2994172bca882275a6af07c8549d969
mpsxina <31672419+mpsxina@users.noreply.github.com>
2019-04-02 16:59:43 -0300

    Initial Upload

---
+++ QueueServiceByAutofac/QueueServiceByAutofac/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+
+namespace QueueServiceByAutofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<PushServive>().As<IPushServive>();
+            builder.RegisterType<PopService>().As<IPopService>();
+            var container = builder.Build();
+
+            container.Resolve<IPushServive>().Execute();
+            container.Resolve<IPopService>().Execute();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QueueServiceByAutofac/QueueServiceByAutofac/QueueService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace QueueServiceByAutofac
+{
+    public interface IPushServive
+    {
+        void Execute();
+    }
+    public class PushServive : IPushServive
+    {
+        public void Execute()
+        {
+            Console.WriteLine("An item is pushed in to the Queue.");
+        }
+    }
+
+    public interface IPopService
+    {
+        void Execute();
+    }
+
+    public class PopService : IPopService
+    {
+        public void Execute()
+        {
+            Console.WriteLine("An item is pop out in to the Queue.");
+        }
+    }
+    class QueueService
+    {
+        public IPushServive ObjPushSerivce = null;
+        public IPopService ObjPopService = null;
+
+        //injection through constructor  
+        public QueueService(IPushServive tmpService)
+        {
+            ObjPushSerivce = tmpService;
+        }
+        //Injection through property  
+        public IPopService SetPopService
+        {
+            set { ObjPopService = value; }
+        }
+        public void Process()
+        {
+            ObjPushSerivce.Execute();
+            ObjPopService.Execute();
+        }
+    }
+}
